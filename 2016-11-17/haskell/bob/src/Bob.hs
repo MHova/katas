52361c8d2b96e47bf89907196321bfc5500abcd1
Michael Ho <mho@sumall.com>
2016-11-17 20:00:51 -0800

    YAAAAAS

--- 2016-11-17/haskell/bob/src/Bob.hs
+++ 2016-11-17/haskell/bob/src/Bob.hs
@@ -1,4 +1,16 @@
 module Bob (responseFor) where
 
+import Data.Char (isAlphaNum, isLetter, isSpace, isUpper)
+import Data.Text (pack, strip)
+import qualified Data.Text as T (last)
+
 responseFor :: String -> String
-responseFor = undefined
+responseFor s
+  | all isSpace s = "Fine. Be that way!"
+  | myAll isUpper $ filter isLetter s = "Whoa, chill out!"
+  | T.last (strip $ pack s) == '?' = "Sure."
+  | otherwise = "Whatever."
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll _ [] = False
+myAll f l = all f l
