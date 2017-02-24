a20fbaf6639c7d2663f6611a7db7fe1953f8a247
Michael Ho <mho@sumall.com>
2017-02-23 19:09:00 -0800

    Step one

--- 2017-02-23/zombicide/src/Main.hs
+++ 2017-02-23/zombicide/src/Main.hs
@@ -1,5 +1,29 @@
 module Main where
 
+type Equipment = String
+
+data Status = Dead | Alive Int
+
+data Survivor =
+  Survivor
+  { name :: String
+  , status :: Status
+  , actions :: Int
+  , inHand :: [Equipment]
+  , reserve :: [Equipment]
+  }
+
+initialSurvivor :: String -> Survivor
+initialSurvivor n = Survivor n (Alive 0) 3 [] []
+
+woundOnce :: Survivor -> Survivor
+woundOnce s@(Survivor _ Dead _ _ _) = s
+woundOnce s@(Survivor _ (Alive wounds) _ _ _)
+  | wounds + 1 >= 2 = s{status = Dead}
+  | otherwise = s{status = Alive (wounds + 1)}
+
+wound :: Survivor -> Int -> Survivor
+wound s wounds = foldl (\b a -> a b) s (replicate wounds woundOnce)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
